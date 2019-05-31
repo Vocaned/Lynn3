11a2c1517bd358c3ce56e69681a511621afabeb5
Fam0r <tmtabletti@gmail.com>
2019-06-01 00:41:21 +0300

    x

---
+++ BotUtils.py
@@ -0,0 +1,9 @@
+import discord
+from datetime import datetime
+from discord.ext import commands
+
+async def embedMsg(channel, hex, title="", description=""):
+    embed = discord.Embed(title=str(title), colour=hex)
+    embed.description = str(description)
+    embed.timestamp = datetime.utcnow()
+    await channel.send(embed=embed, content='')
