fd6336d58868f134a6d98daa7295441bf94872a1
Fam0r <tmtabletti@gmail.com>
2020-01-14 10:41:32 +0200

    More

--- extensions/Fun.py
+++ extensions/Fun.py
@@ -6,11 +6,11 @@ from urllib import parse
 from BotUtils import REST
 import asyncio
 
-"""Games and stuff"""
+'''Games and stuff'''
 
 
 class Fun(commands.Cog):
-	"""Fun"""
+	'''Fun'''
 
 	def __init__(self, bot):
 		self.bot = bot
@@ -71,8 +71,8 @@ class Fun(commands.Cog):
 
 	@commands.command(name='minesweeper')
 	async def minesweeper(self, ctx, *, args=None):
-		"""Generates a game of minesweeper.
-		args = `width height bombs (nospoil)`"""
+		'''Generates a game of minesweeper.
+		args = `width height bombs (nospoil)`'''
 		spoil = True
 		if args != None and len(str(args).split(' ')) > 2:
 			width = int(str(args).split(' ')[0])
@@ -114,60 +114,59 @@ class Fun(commands.Cog):
 		embed = discord.Embed(title='Minesweeper', colour=0xFFA500)
 		embed.description = output
 		embed.timestamp = datetime.utcnow()
-		await ctx.send(embed=embed, content="")
+		await ctx.send(embed=embed, content='')
 
-	multiNames = ["\U0001F1E6", "\U0001F1E7", "\U0001F1E8", "\U0001F1E9"]
-	boolNames = ["\U00002714", "\U00002716"]
+	multiNames = ['\U0001F1E6', '\U0001F1E7', '\U0001F1E8', '\U0001F1E9']
+	boolNames = ['\U00002714', '\U00002716']
 
 	@commands.command(name='trivia')
-	async def trivia(self, ctx, *, difficulty="random", streak=0):
-		"""Generates trivia questions.
+	async def trivia(self, ctx, *, difficulty='random', streak=0):
+		'''Generates trivia questions.
 		Difficulty can be Easy, Medium, Hard or Random
 		(default: Random)
-		Ignore the streak parameter, thanks :)"""
+		Ignore the streak parameter, thanks :)'''
 
 		if streak != 0 and ctx.message:
-			raise commands.CommandError("%Too many arguments. Are you trying to cheat?")
+			raise commands.CommandError('%Too many arguments. Are you trying to cheat?')
 
-		validdiffs = ("easy", "medium", "hard")
-		if difficulty.lower() in validdiffs:
-			dif = "&difficulty=" + difficulty.lower()
-		elif difficulty.lower() == "random":
-			dif = ""
+		if difficulty.lower() in ('easy', 'medium', 'hard'):
+			dif = '&difficulty='+difficulty.lower()
+		elif difficulty.lower() == 'random':
+			dif = ''
 		else:
-			raise commands.CommandError("%Invalid difficulty")
+			raise commands.CommandError('%Invalid difficulty')
 
-		data = await REST("https://opentdb.com/api.php?amount=1&encode=url3986"+dif)
-		if data["response_code"] != 0:
-			raise commands.CommandError("%API error " + str(data["response_code"]))
+		data = await REST('https://opentdb.com/api.php?amount=1&encode=url3986'+dif)
+		if data['response_code'] != 0:
+			raise commands.CommandError(f'%API error '+str(data['response_code']))
 
-		data = data["results"][0]
+		data = data['results'][0]
 
-		if data["type"] == "multiple":
-			answers = [data["correct_answer"]] + data["incorrect_answers"]
+		if data['type'] == 'multiple':
+			answers = [data['correct_answer']] + data['incorrect_answers']
 			random.shuffle(answers)
 		else:
-			answers = ["True", "False"]
-		ansIndex = answers.index(data["correct_answer"])
+			answers = ['True', 'False']
+		ansIndex = answers.index(data['correct_answer'])
 
-		if data["difficulty"] == "easy":
+		if data['difficulty'] == 'easy':
 			col = 0x00FF00
-		elif data["difficulty"] == "medium":
+		elif data['difficulty'] == 'medium':
 			col = 0xFFFF00
-		elif data["difficulty"] == "hard":
+		elif data['difficulty'] == 'hard':
 			col = 0xFF0000
 
-		embed = discord.Embed(title='Trivia - ' + parse.unquote(data["category"]), color=col)
-		embed.description = "```" + parse.unquote(data["question"]) + "```"
-		embed.set_footer(text="Difficulty: " + data["difficulty"] + ". Data provided by Open Trivia DB (PixelTail Games)")
+		embed = discord.Embed(title=f'Trivia - {parse.unquote(data["category"])}', color=col)
+		embed.description = f'```{parse.unquote(data["question"])}```'
+		embed.set_footer(text=f'Difficulty: {data["difficulty"]}. Data provided by Open Trivia DB (PixelTail Games)')
 
-		if data["type"] == "multiple":
+		if data['type'] == 'multiple':
 			for i in range(4):
 				embed.add_field(name=self.multiNames[i], value=parse.unquote(answers[i]))
 			nameList = self.multiNames
-		elif data["type"] == "boolean":
-			embed.add_field(name=self.boolNames[0], value="True")
-			embed.add_field(name=self.boolNames[1], value="False")
+		elif data['type'] == 'boolean':
+			embed.add_field(name=self.boolNames[0], value='True')
+			embed.add_field(name=self.boolNames[1], value='False')
 			nameList = self.boolNames
 		msg = await ctx.send(embed=embed)
 		for name in nameList:
@@ -179,20 +178,20 @@ class Fun(commands.Cog):
 			r,u = await self.bot.wait_for('reaction_add', timeout=20.0, check=check)
 			guessIndex = nameList.index(str(r))
 			if guessIndex == ansIndex:
-				results = "Correct!\n" + nameList[ansIndex] + " - " + parse.unquote(data["correct_answer"])
+				results = f'Correct!\n{nameList[ansIndex]} - {parse.unquote(data["correct_answer"])}'
 				streak += 1
 				correct = True
 			else:
-				results = "Wrong!\nYou picked " + nameList[guessIndex] + " - " + parse.unquote(answers[guessIndex]) + "\nThe answer was " + nameList[ansIndex] + " - " + parse.unquote(data["correct_answer"]) + "\n"
+				results = f'Wrong!\nYou picked {nameList[guessIndex]} - {parse.unquote(answers[guessIndex])}\nThe answer was {nameList[ansIndex]} - {parse.unquote(data["correct_answer"])}\n'
 				correct = False
 		except asyncio.TimeoutError:
-			results = "Ran out of time!\nThe answer was " + nameList[ansIndex] + " - " + parse.unquote(data["correct_answer"])
+			results = f'Ran out of time!\nThe answer was {nameList[ansIndex]} - {parse.unquote(data["correct_answer"])}'
 			correct = False
 		hadStreak = False
 		if streak != 0:
 			hadStreak = True
-			endStreak = "\n\nYour streak ended with " + str(streak)+"x"
-			curStreak = "\n\nYou currently have a " + str(streak) + "x streak"
+			endStreak = f'\n\nYour streak ended with {str(streak)}x streak'
+			curStreak = f'\n\nYou currently have a {str(streak)}x streak'
 			if correct:
 				fullResults = results + curStreak
 			else:
@@ -201,11 +200,11 @@ class Fun(commands.Cog):
 		else:
 			fullResults = results
 
-		resultsMsg = await ctx.send(fullResults+"\nClick \U0001F501 within 10 seconds to continue.")
-		await resultsMsg.add_reaction("\U0001F501")
+		resultsMsg = await ctx.send(f'{fullResults}\nClick \U0001F501 within 10 seconds to continue.')
+		await resultsMsg.add_reaction('\U0001F501')
 
 		def check2(reaction, user):
-			return user == ctx.author and reaction.message.id == resultsMsg.id and str(reaction.emoji) == "\U0001F501"
+			return user == ctx.author and reaction.message.id == resultsMsg.id and str(reaction.emoji) == '\U0001F501'
 		try:
 			r,u = await self.bot.wait_for('reaction_add', timeout=10.0, check=check2)
 			ctx.message = None
@@ -214,7 +213,7 @@ class Fun(commands.Cog):
 			if hadStreak:
 				results += endStreak
 			await resultsMsg.edit(content=results)
-			await resultsMsg.remove_reaction("\U0001F501", self.bot.user)
+			await resultsMsg.remove_reaction('\U0001F501', self.bot.user)
 
 
 def setup(bot):
--- extensions/Misc.py
+++ extensions/Misc.py
@@ -7,22 +7,22 @@ import subprocess
 import sys
 from BotUtils import splitMessage
 
-"""Misc commands"""
+'''Misc commands'''
 
 
 class Misc(commands.Cog):
-    """Misc"""
+    '''Misc'''
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["col", "color", "colour"])
+    @commands.command(aliases=['col', 'color', 'colour'])
     async def hex(self, ctx, *, col):
-        hexcol = str(col).replace("#", "")
+        hexcol = str(col).replace('#', '')
         if not re.search(r'^(?:[0-9a-fA-F]{3}){1,2}$', hexcol):
-            raise commands.CommandError(message="Invalid hex color")
+            raise commands.CommandError(message='Invalid hex color')
 
-        embed = discord.Embed(title="#"+hexcol, colour=int(hexcol, 16))
+        embed = discord.Embed(title='#'+hexcol, colour=int(hexcol, 16))
         r = int(hexcol[:2], 16)
         rp = round(int(hexcol[:2], 16) / 2.55, 2)
         g = int(hexcol[2:4], 16)
@@ -34,19 +34,19 @@ class Misc(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        embed = discord.Embed(title="Pong!", colour=0xfffdd0)
-        embed.description = str(round(self.bot.latency*1000, 2)) + "ms"
+        embed = discord.Embed(title='Pong!', colour=0xfffdd0)
+        embed.description = f'{str(round(self.bot.latency*1000, 2))}ms'
         await ctx.send(embed=embed, content='')
 
-    @commands.command(aliases=["remind", "remindme"])
+    @commands.command(aliases=['remind', 'remindme'])
     async def reminder(self, ctx, *message):
-        """Sets a reminder.
+        '''Sets a reminder.
         Time must be in ISO 8601 format. Times are in UTC. Examples:
         %reminder 2017-01-10T12:30:59 Example message    =January 10th 2017, 12:30:59 UTC
         %reminder 2026-05-09T20:00:00 Example message    =May 9th 2026, 8 pm UTC
         %reminder Example message                      =No time specified
-        """
-        embed = discord.Embed(title="Reminder", colour=0x8630bf)
+        '''
+        embed = discord.Embed(title='Reminder', colour=0x8630bf)
         if len(message) > 1:
             try:
                 date = datetime.fromisoformat(message[0])
@@ -54,32 +54,32 @@ class Misc(commands.Cog):
                 message = message[1:]
             except:
                 pass
-        embed.description = " ".join(message)
-        embed.set_footer(text=str(ctx.message.author.name) + '#' +  str(ctx.message.author.discriminator), icon_url=ctx.message.author.avatar_url)
+        embed.description = ' '.join(message)
+        embed.set_footer(text=f'{str(ctx.message.author.name)}#{str(ctx.message.author.discriminator)}', icon_url=ctx.message.author.avatar_url)
         await ctx.message.delete()
         await ctx.send(embed=embed, content='')
 
     @commands.command()
     async def whois(self, ctx, *, domain):
-        """Whois"""
+        '''Whois'''
         if not sys.platform.startswith('linux'):
-            await ctx.send("This command is only usable when the bot is hosted on linux. Sorry!")
+            await ctx.send('This command is only usable when the bot is hosted on linux. Sorry!')
             return
-        output = subprocess.check_output(["whois", domain], stderr=subprocess.PIPE).decode("utf-8")
+        output = subprocess.check_output(['whois', domain], stderr=subprocess.PIPE).decode('utf-8')
 
         for msg in splitMessage(output):
             await ctx.send(msg)
 
-    @commands.command(aliases=["id"])
+    @commands.command(aliases=['id'])
     async def snowflake(self, ctx, *, snowflake):
         # HACK: Parse mentions by just removing everything that's not a digit
-        snowflake = "".join(c for c in snowflake if c.isdigit())
+        snowflake = ''.join(c for c in snowflake if c.isdigit())
         snowflake = int(snowflake)
-        embed = discord.Embed(title="Snowflake " + bin(snowflake)[2:], colour=0x7289DA)
-        embed.description = "**Timestamp**: " + datetime.utcfromtimestamp(((snowflake >> 22) + 1420070400000) / 1000).strftime('%c') \
-                          + "\n**Internal worker ID**: " + str((snowflake & 0x3E0000) >> 17) \
-                          + "\n**Internal process ID**: " + str((snowflake & 0x1F000) >> 12) \
-                          + "\n**Increment**: " + str(snowflake & 0xFFF)
+        embed = discord.Embed(title=f'Snowflake {snowflake}', colour=0x7289DA)
+        embed.description = f'''**Timestamp**: {datetime.utcfromtimestamp(((snowflake >> 22) + 1420070400000) / 1000).strftime('%c')}
+                                **Internal worker ID**: {str((snowflake & 0x3E0000) >> 17)}
+                                **Internal process ID**: {str((snowflake & 0x1F000) >> 12)}
+                                **Increment**: {str(snowflake & 0xFFF)}'''
         await ctx.send(embed=embed, content='')
 
 def setup(bot):
