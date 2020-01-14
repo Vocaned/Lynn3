80536153c736081eaee8202a6bb676125975aecb
Fam0r <tmtabletti@gmail.com>
2020-01-14 10:21:00 +0200

    Start fixing the formatting
    Also fix errors when the bot tries to do something it doesn't have permission to do

--- extensions/Mod.py
+++ extensions/Mod.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 class Mod(commands.Cog):
-    """Mod"""
+    '''Mod'''
 
     def __init__(self, bot):
         self.bot = bot
@@ -18,105 +18,113 @@ class Mod(commands.Cog):
                 entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
 
             if entity is None:
-                raise commands.BadArgument("%Not a valid previously-banned member.")
+                raise commands.BadArgument('%Not a valid previously-banned member.')
             return entity
 
-    @commands.command(name="hackban")
+    @commands.command(name='hackban')
     @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
     @commands.guild_only()
     async def hackban(self, ctx, *, uId):
-        """Bans an user that's not in the server"""
+        '''Bans an user that's not in the server'''
         try:
-            await ctx.guild.ban(discord.Object(id=uId), reason="Hackbanned by " + ctx.author.name)
+            await ctx.guild.ban(discord.Object(id=uId), reason=f'Hackbanned by {ctx.author.name}')
         except discord.Forbidden:
-            await ctx.send("Could not hackban " + uId)
+            await ctx.send(f'Could not hackban {uId}')
         else:
-            await ctx.send(uId + " was hackbanned.")
+            await ctx.send(f'{uId} was hackbanned.')
 
-    @commands.command(name="ban")
+    @commands.command(name='ban')
     @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
     @commands.guild_only()
     async def ban(self, ctx, *, user: discord.Member):
-        """Bans an user"""
+        '''Bans an user'''
         try:
-            await ctx.guild.ban(user, reason="Banned by " + ctx.author.name)
+            await ctx.guild.ban(user, reason=f'Banned by {ctx.author.name}')
         except discord.Forbidden:
-            await ctx.send("Could not ban " + user.name)
+            await ctx.send(f'Could not ban {user.name}')
         else:
-            await ctx.send(user.name + " was banned.")
+            await ctx.send(f'{user.name} was banned.')
 
-    @commands.command(name="unban")
+    @commands.command(name='unban')
     @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
     @commands.guild_only()
     async def unban(self, ctx, *, user: BannedMember):
-        """Unbans an user"""
+        '''Unbans an user'''
         try:
-            await ctx.guild.unban(user.user, reason="Unbanned by " + ctx.author.name)
+            await ctx.guild.unban(user.user, reason=f'Unbanned by {ctx.author.name}')
         except discord.Forbidden:
-            await ctx.send("Could not unban " + user.name)
+            await ctx.send(f'Could not unban {user.name}')
         else:
-            await ctx.send(user.name + " was unbanned.")
+            await ctx.send(f'{user.name} was unbanned.')
 
-    @commands.command(name="kick")
+    @commands.command(name='kick')
     @commands.has_permissions(kick_members=True)
+    @commands.bot_has_permissions(kick_members=True)
     @commands.guild_only()
     async def kick(self, ctx, *, user: discord.Member):
-        """Kicks an user"""
+        '''Kicks an user'''
         try:
-            await ctx.guild.kick(user, reason="Kicked by " + ctx.author.name)
+            await ctx.guild.kick(user, reason=f'Kicked by {ctx.author.name}')
         except discord.Forbidden:
-            await ctx.send("Could not kick " + user.name)
+            await ctx.send(f'Could not kick {user.name}')
         else:
-            await ctx.send(user.name + " was kicked.")
+            await ctx.send(f'{user.name} was kicked.')
 
-    @commands.command(name="nick", aliases=["nickname"])
+    @commands.command(name='nick', aliases=['nickname'])
     @commands.has_permissions(change_nicknames=True)
+    @commands.bot_has_permissions(change_nicknames=True)
     @commands.guild_only()
-    async def nick(self, ctx, user: discord.Member, *, nick=""):
-        """Changes user's nickname"""
+    async def nick(self, ctx, user: discord.Member, *, nick=''):
+        '''Changes user's nickname'''
         try:
             await user.edit(nick=nick)
         except discord.Forbidden:
-            await ctx.send("Could not change " + user.name + "'s nickname")
+            await ctx.send(f'Could not change {user.name}\'s nickname')
         else:
-            await ctx.send(user.name + "'s nickname was changed to `"+nick+"`")
+            await ctx.send(f'{user.name}\'s nickname was changed to `{nick}`')
 
-    @commands.command(name="role")
+    @commands.command(name='role')
     @commands.has_permissions(manage_roles=True)
+    @commands.bot_has_permissions(manage_roles=True)
     @commands.guild_only()
     async def role(self, ctx, user: discord.Member, *, role: discord.Role):
-        """Add or remove role from user"""
+        '''Add or remove role from user'''
         if role in user.roles:
             try:
                 await user.remove_roles(role)
             except discord.Forbidden:
-                await ctx.send("Could not remove " + user.name + "'s role")
+                await ctx.send(f'Could not remove {user.name}\'s role')
             else:
-                await ctx.send(user.name + " was removed from the role " + role.name)
+                await ctx.send(f'{user.name}\'s {role.name} role was removed')
         else:
             try:
                 await user.add_roles(role)
             except discord.Forbidden:
-                await ctx.send("Could not add a role to " + user.name)
+                await ctx.send(f'Could not add a role to {user.name}')
             else:
-                await ctx.send(user.name + " was given the role " + role.name)
+                await ctx.send(f'{user.name} was given the role {role.name}')
 
-    @commands.command(name="createinvite", aliases=["makeinv"])
+    @commands.command(name='createinvite', aliases=['makeinv'])
     @commands.has_permissions(create_instant_invite=True)
+    @commands.bot_has_permissions(create_instant_invite=True)
     @commands.guild_only()
     async def createinvite(self, ctx, limit=0, duration_minutes=0):
         inv = await ctx.channel.create_invite(max_age=duration_minutes*60, max_uses=limit)
-        info = "Invite lasts for `"+ (str(inv.max_age // 60) if inv.max_age else "∞") + "` minutes.\n" \
-        + "Invite can be used `"+ (str(inv.max_uses) if inv.max_uses else "∞") + "` times."
-        await ctx.send("Invite created: https://discord.gg/" + inv.code + "\n" + info)
+        info = f'''Invite lasts for `{(str(inv.max_age // 60) if inv.max_age else '∞')}` minutes.
+        Invite can be used `{(str(inv.max_uses) if inv.max_uses else '∞')}` times.'''
+        await ctx.send(f'Invite created: https://discord.gg/{inv.code}\n{info}')
 
     @commands.command(name='purge', aliases=['prune'])
     @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
     @commands.guild_only()
     async def purge(self, ctx, amount: int=None):
-        """Purge an amount of messages in a channel"""
+        '''Purge an amount of messages in a channel'''
         if amount>500 or amount<0:
-            raise commands.CommandError(message="Invalid amount")
+            raise commands.CommandError(message='Invalid amount')
         await ctx.message.delete()
         await ctx.message.channel.purge(limit=amount)
         await ctx.send(f'Sucesfully deleted **{int(amount)}** messages!', delete_after=5)
@@ -125,7 +133,7 @@ class Mod(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     @commands.guild_only()
     async def echo(self, ctx, *message):
-        await ctx.send(" ".join(message))
+        await ctx.send(' '.join(message))
 
 def setup(bot):
     bot.add_cog(Mod(bot))
