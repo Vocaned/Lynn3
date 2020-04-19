bacbaa7e296b4def1e37470b435d810656cea672
Fam0r <tmtabletti@gmail.com>
2020-04-19 11:41:20 +0300

    Oops accidentally pushed this

--- config.example.py
+++ config.example.py
@@ -22,7 +22,7 @@ def get_prefix(bot, message):
 apiKeys = {
     'tracker':          '[KEY]',
     'steam':            '[KEY]',
-    'openweathermap':   '[KEY]',
+    'darksky':          '[KEY]',
     'omdb':             '[KEY]',
     'twitterConsKey':   '[KEY]',
     'twitterConsSecret':'[KEY]',
