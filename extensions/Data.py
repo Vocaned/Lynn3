a92110ad8a9c3ded0eb8a9b8b02a553669bfa5a6
Fam0r <tmtabletti@gmail.com>
2020-04-20 23:23:21 +0300

    Hex data support

--- extensions/Data.py
+++ extensions/Data.py
@@ -9,14 +9,15 @@ class Data(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def sanitize(self, string: bytes) -> str:
-        return string.decode('utf-8', 'ignore').replace('```', '`\U00002063``')
+    async def send(self, ctx, string: bytes) -> str:
+        await ctx.send(ctx.author.mention + '```' + string.decode('utf-8', 'ignore').replace('```', '`\U00002063``') + '```')
 
     # BASE
     @commands.group(name='decode')
     async def decode(self, ctx):
         if not ctx.invoked_subcommand:
             raise commands.UserInputError()
+
     
     @commands.group(name='encode')
     async def encode(self, ctx):
@@ -27,13 +28,27 @@ class Data(commands.Cog):
     @decode.command(name='base64', aliases=['b64'])
     async def dbase64(self, ctx, *, val):
         try:
-            await ctx.send(f'{ctx.author.mention}, ```{self.sanitize(base64.b64decode(val.encode()))}```')
+            await self.send(ctx, base64.b64decode(val.encode()))
         except binascii.Error:
             await ctx.send('Could not parse base64')
 
     @encode.command(name='base64', aliases=['b64'])
     async def ebase64(self, ctx, *, val):
-        await ctx.send(f'{ctx.author.mention}, ```{self.sanitize(base64.b64encode(val.encode()))}```')
+        await self.send(ctx, base64.b64encode(val.encode()))
+
+    #HEX
+    @decode.command(name='hex', aliases=['base8', 'b8'])
+    async def dhex(self, ctx, *, val):
+        try:
+            await self.send(ctx, bytes.fromhex(val))
+        except:
+            await ctx.send('Could not parse hex')
+        
+    @encode.command(name='hex', aliases=['base8', 'b8'])
+    async def ehex(self, ctx, *, val):
+        await self.send(ctx, binascii.hexlify(val.encode()))
+
+
 
 
 def setup(bot):
