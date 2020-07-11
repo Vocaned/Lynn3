25d9f4703d8fd655d88cd68e46a8bf4942612bdf
Fam0r <tmtabletti@gmail.com>
2020-07-11 14:58:15 +0300

    Properly parse time now

--- cogs/misc/reminders.py
+++ cogs/misc/reminders.py
@@ -15,10 +15,15 @@ class Reminders(commands.Cog):
         '''Sets a reminder.
         Times are in UTC by default'''
 
-        date = search_dates(message, settings={"TIMEZONE": "UTC"})
+        date = search_dates(' '.join(message), settings={"TIMEZONE": "UTC"})
+        if not date:
+            await ctx.send('No date or time found in your message')
+            return
+        elif len(date) > 1:
+            await ctx.send(f'Multiple dates found in your message. Using the first one ("{date[0][0]}")')
         date = date[0]
-        await ctx.send("I'll remind you on " + date.strftime("%c") + "!\n*(please dont use this command for anything super important, reminders might just fail and never actually remind you because I don't have the motivation to make this bot good)*")
-        await asyncio.sleep((date-datetime.utcnow()).total_seconds())
+        await ctx.send("I'll remind you on " + date[1].strftime("%c") + "!\n*(please dont use this command for anything super important, reminders might just fail and never actually remind you because I don't have the motivation to make this bot good)*")
+        await asyncio.sleep((date[1]-datetime.utcnow()).total_seconds())
         
         embed = discord.Embed(title='Reminder', colour=0x8630bf)
         embed.description = ' '.join(message)
