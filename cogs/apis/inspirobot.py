a03c67a6e62f64cf39a7fd1e893e5248382927a1
Fam0r <tmtabletti@gmail.com>
2020-09-01 00:14:07 +0300

    inspirobot api

---
+++ cogs/apis/inspirobot.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+import discord
+from BotUtils import REST
+
+class inspirobot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='inspire', aliases=['inspirobot', 'inspireme'])
+    async def inspirobotAPI(self, ctx):
+        """Generates an inspirational quote"""
+        data = await REST('https://inspirobot.me/api?generate=true', returns='raw')
+        embed = discord.Embed(color=0x00af00)
+        embed.set_image(url=data)
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(inspirobot(bot))
