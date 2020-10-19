d9102e8004c942aca55cd6555e0eb693d28fe2e9
Fam0r <tmtabletti@gmail.com>
2020-10-19 21:21:58 +0300

    Use new shell in %git

--- cogs/admin/git.py
+++ cogs/admin/git.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
-from BotUtils import splitMessage
-import subprocess
+from BotUtils import splitMessage, shellCommand
 
 class Git(commands.Cog):
     def __init__(self, bot):
@@ -14,33 +13,15 @@ class Git(commands.Cog):
 
     @git.command(name='reset', aliases=['fuck'])
     async def gitreset(self, ctx):
-        p = subprocess.check_output(['git', 'reset', '--hard', 'origin/master'], stderr=subprocess.STDOUT, timeout=30).decode('utf-8')
-        await ctx.send(splitMessage(p)[0])
+        await shellCommand(ctx, 'git reset --hard origin/master', timeout=30)
 
     @git.command(name='pull')
     async def gitpull(self, ctx):
-        p = subprocess.check_output(['git', 'pull'], stderr=subprocess.STDOUT, timeout=30).decode('utf-8')
-        w = p.split()
-        try:
-            commits = w[w.index('Updating') + 1]
-
-            p2 = subprocess.check_output(['git', 'log', '--format=%h %an | %B%n%N', commits], stderr=subprocess.STDOUT, timeout=30)
-            p2 = '\n'.join([line for line in p2.decode('utf-8').split('\n') if line.strip() != ''])
-        except:
-            p2 = ''
-        # 1993 = 2000 - len('```\n```')
-        if len(p) + len(p2) >= 1993:
-            if len(p) >= 1994:
-                await ctx.send('Git output too long. Changes were still applied.')
-            else:
-                await ctx.send('```' + p + '```')
-        else:
-            await ctx.send('```' + p + '\n' + p2 + '```')
+        await shellCommand(ctx, 'git pull && git log @{1}.. --format="%h %an | %B%n%N"', timeout=30)
 
     @git.command(name='log', aliases=['show'])
     async def gitlog(self, ctx, last='1'):
-        p = subprocess.check_output(['git', 'log', '-'+last], stderr=subprocess.STDOUT, timeout=30).decode('utf-8')
-        await ctx.send('```' + p[:1994] + '```')
+        await shellCommand(ctx, 'git log -'+str(int(last)), timeout=30)
 
 def setup(bot):
     bot.add_cog(Git(bot))
