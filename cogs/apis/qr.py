62cca4d7848516c50caeee99cb9ebaa4bc3b2f2a
Fam0r <tmtabletti@gmail.com>
2020-10-19 21:12:57 +0300

    QR code creation command

---
+++ cogs/apis/qr.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+from BotUtils import REST, escapeURL
+import discord
+
+class QR(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='qr')
+    async def QR(self, ctx, *, text):
+        embed = discord.Embed()
+        embed.set_thumbnail(url=f'https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=512x512&chl={escapeURL(text)}')
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(QR(bot))
