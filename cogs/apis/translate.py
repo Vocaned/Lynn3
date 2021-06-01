03bf7dc0f1ec7e459668f39dfd283f1b8504dd11
Fam0r <fam0r@mailbox.org>
2021-06-01 20:32:19 +0300

    better translate command

--- cogs/apis/translate.py
+++ cogs/apis/translate.py
@@ -7,15 +7,20 @@ class Translate(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='translate')
+    @commands.command(name='translate', aliases=['trans'])
     async def TranslateAPI(self, ctx, *, query):
         """Translate stuff using Google Translate.
-        Only Auto-Detect -> English right now im too lazy to make this bot any good LOL !"""
-        # Defaults to Auto-Detect -> English
-        # use 2 letter country codes i guess lol too lazy to make this bot any good
+        Defaults to Auto-Detect -> English
+        use 2 letter country codes i guess lol too lazy to make this bot any good"""
 
         inlang = 'auto'
         outlang = 'en'
+
+        for word in query:
+            if word.startswith('from:') and word != 'from:': # check that there's no space after from:
+                inlang = word.split(':')[1]
+            if word.startswith('to:') and word != 'to:':
+                outlang = word.split(':')[1]
 
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
         data = await REST(f'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl={inlang}&tl={outlang}&q={query}', headers=headers)
@@ -24,7 +29,7 @@ class Translate(commands.Cog):
 
         confidence = ''
         if data[6] and data[6] != 1:
-            confidence = f'(confidence: {data[6]*100}%)'
+            confidence = f'(confidence: {round(data[6]*100)}%)'
 
         await ctx.send(f'{fromlang} -> {outlang}: {data[0][0][0]} {confidence}')
 
